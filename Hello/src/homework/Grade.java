7e6bd3726881b8fe4f3cf40ffe87981554c58ee8
sd8599 <167828768+sd8599@users.noreply.github.com>
2024-05-10 10:00:33 +0900

    Grade.java 업데이트

--- Hello/src/homework/Grade.java
+++ Hello/src/homework/Grade.java
@@ -24,8 +24,8 @@ double mathScore) {
 	
 	public static void main(String[] args) {
 		
-		Grade grade1= new Grade("강백호",90.0,85.5,70.0);
-		Grade grade2= new Grade("채치수",82.0,92.0,60.5);
+	Grade grade1= new Grade("강백호",90.0,85.5,70.0);
+	Grade grade2= new Grade("채치수",82.0,92.0,60.5);
 		
 		System.out.println("강백호의 평균점수 
 : "+grade1.calculateAverage());
